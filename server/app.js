45b4765a2ea499b63f018ecded3edb94d4796b8c
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2024-11-04 10:23:28 +0530

    backend changed

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const adminRouter = require("./routes/admins");
 const cors = require("cors");
 // const uri ="mongodb://localhost:27020,localhost:27021,localhost:27022/CBIT?replicaSet=m101";
 const uri = "mongodb://localhost:27017/CBIT";
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -20,6 +20,6 @@ con.on("open", () => {
 app.use("/students", StudentRouter);
 app.use("/admins", adminRouter);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log("server started on",port);
 });
